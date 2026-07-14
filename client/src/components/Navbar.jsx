a7aa464d4fe0a8287c9b13611790c56204a2db42
mac <mac@hosseins-mbp.home>
2026-07-15 01:20:39 +0200

    start building project

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>PArts Navbar</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+// import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <Navbar />
+
+      <h1>Welcome to PArts</h1>
+      <p>hello world!</p>
+    </div>
+  );
+}
+
+export default HomePage;
