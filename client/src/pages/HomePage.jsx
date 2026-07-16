01df8f5a88c224f63b38ab181e20b4d82b9fab99
mac <mac@hosseins-mbp.home>
2026-07-17 00:29:03 +0200

    add hero and portfolio component with mock data

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
 // import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Portfolio from "../components/Portfolio";
 import "./HomePage.css";
 
 function HomePage() {
   return (
     <div className="home-page">
       <Navbar />
-
-      <h1>Welcome to PArts</h1>
-      <p>hello world!</p>
+      <Hero />
+      <Portfolio />
     </div>
   );
 }
